fbc90442fa5f8fd93f0bea3df385ebc420919314
Davi Aredes <daviaredes@gmail.com>
2022-12-06 17:24:05 -0300

    06/12/22 fim

--- jokenpo/jokenpo.py
+++ jokenpo/jokenpo.py
@@ -1,22 +1,22 @@
 from random import choice
-from time import sleep
 
 vitoria = 0
 derrota = 0
 empate = 0
 
-
 def contador(vitoria, derrota, empate):
-
     cont = str(input('Quer continuar jogando, Diguite sim para continuar e nao para sair:').lower().strip()[0])
-    sleep(1)
 
     if cont == 's':
         main(vitoria, derrota, empate)  
     elif cont == 'n':
         rodadas = (vitoria + derrota) + empate
-        print(f'Obrigado por jogar em nossa maquina Voce jogou {rodadas} rodadas Ganhou em {vitoria} delas, Empatou em {empate} delas e Perdeu em {derrota} delas')
-        
+        print(f'Obrigado por jogar em nossa maquina Voce jogou {rodadas} rodadas Ganhou em {vitoria} delas Empatou em {empate} delas e Perdeu em {derrota} delas')
+    elif contador(vitoria, derrota, empate) == 's' or 'n':
+        pass
+    else:
+        contador(vitoria, derrota, empate)
+
 
 def main(vitoria, derrota, empate):
 
@@ -25,7 +25,7 @@ def main(vitoria, derrota, empate):
     bot = choice(['papel', 'pedra', 'tesoura'])
 
     while True:
-
+        
         player = str(input('Opçao escolha: ').lower().strip())
         if player == "papel" or "pedra" or "tesoura":
             pass
@@ -33,19 +33,17 @@ def main(vitoria, derrota, empate):
         if player == bot:
             print('Empate os dois ganharam:')
             empate += 1
-            sleep(1)
             contador(vitoria, derrota, empate)
 
         elif player == "pedra" and bot == "tesoura" or player == "papel" and bot == "pedra" or player == "tesoura" and bot == "papel":
             print('Parabens voce ganhou')
             vitoria += 1
-            sleep(1)
             contador(vitoria, derrota, empate)
 
         elif bot == "pedra" and player == "tesoura" or bot == "papel" and player == "pedra" or bot == "tesoura" and player == "papel":
             print('Voce perdeu ')
             derrota += 1
-            sleep(1)
             contador(vitoria, derrota, empate)
+        break
 
 main(vitoria, derrota, empate)
