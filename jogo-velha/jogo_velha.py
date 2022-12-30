037cb77a67157635fbae0d0de5b334bf8313a112
Davi Aredes <daviaredes@gmail.com>
2022-12-30 12:36:24 -0300

    new sintaxe 30/12/22

--- jogo-velha/jogo_velha.py
+++ jogo-velha/jogo_velha.py
@@ -1,41 +1,27 @@
-from random import randint
+from PySimpleGUI import *
 
-escolhidos = [ ]
+theme('DarkGrey14')
 
-def painel():
-    player = int(input('Escolha: '))
-    escolhidos.append(player)
-global player
+layout = [
+    [Checkbox('X'), Txt('OU'), Checkbox('O')],
+    [Button(), Button(), Button()],
+    [Button(), Button(), Button()],
+    [Button(), Button(), Button()],
+    [Button('Jogar')],
+]
 
+Jan = FlexForm('', layout=layout, default_button_element_size=(5, 2), auto_size_buttons=False)
 
-def main():
-    print('Jogo da Velha')
-    print('|1|2|3|\n|4|5|6|\n|7|8|9|') 
-    print('O Numero no qual voce escolher ira ser marcar com um  X')
-    while True:
+Valor= ''
+while True:
 
-        painel()
+    button, values = Jan.Read()
 
-        bot = randint(1, 10)
-        print(bot)
-global bot
-
-mecanicas()
-
-
-def mecanicas():
-   
-    layout = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-    if player or bot == escolhidos:
-        if player:
-            print('Opçao ja escolida esconha outro numero')
-        if bot:
-            bot = randint(1, 10)    
-    if player:
-        n = player - 1
-        layout.append('X'[n])
-        return print(f'|{layout[0]}|{layout[1]}|{layout[2]}|\n|{layout[3]}|{layout[4]}|{layout[5]}|\n|{layout[6]}|{layout[7]}|{layout[8]}|')
-
-main()
-# 753, 159, 258, 147, 258,369
+    # teste
+    if Valor == '':
+        print('ola')
+    if button == None:
+        break
+    else:
+        Valor += button
+        print(Valor)
