4656d47444c13ffeddf443167bcb8566646f297b
Davi Aredes <daviaredes@gmail.com>
2023-01-02 18:00:39 -0300

    modifcacao

--- jogo-velha/jogo_velha.py
+++ jogo-velha/jogo_velha.py
@@ -2,39 +2,29 @@ from PySimpleGUI import *
 
 theme('DarkGrey14')
 
-
-def opçao():
-    layout = [
-        [Button('X'), Text('OU'), Button('O')]
-    ]
-
-    return Window('Opçao', layout=layout, default_button_element_size=(5, 2), auto_size_buttons=False) 
-
-
-Window_popup = opçao()
-
-
 layout = [
-    [Button(), Button(), Button()],
-    [Button(), Button(), Button()],
-    [Button(), Button(), Button()],
-    [Submit('Jogar', key='continuar')],
+    [Button(k='1'), Button(k='2'), Button(k='3')],
+    [Button(k='4'), Button(k='5'), Button(k='6')],
+    [Button(k='7'), Button(k='8'), Button(k='9')],
+    [Txt('' * 10)],
+    [Button(k='subm')]
 ]
-
-
-Jan = FlexForm('', layout=layout, default_button_element_size=(5, 2), auto_size_buttons=False)
-
-Valor = ''
+Windo = FlexForm('Jogo', default_button_element_size=(5, 2),  auto_size_buttons=False, layout=layout)
 
 while True:
 
-    button, values = Jan.Read()
-           
-    if Valor == 'continuar':
-        Window_popup()
+    button, values = Windo.Read()
 
     if button == None:
         break
-    else:
-        Valor += button
-        
+    if button == 'subm':
+
+
+
+'''
+    if button == '1':
+        print('ansjhewdgegw')
+
+    if button == 'subm':
+        popup('FOI')
+'''
