43573c88e3e6a3f4bd7540502123f73fdc75641b
Davi Aredes <daviaredes@gmail.com>
2023-01-11 15:03:54 -0300

    jogada bot limitador jogadas

--- jogo-velha/jogo_velha.py
+++ jogo-velha/jogo_velha.py
@@ -4,7 +4,7 @@ from random import randint
 theme('DarkGrey14')
 
 layout = [
-    [ReadFormButton('', k='1'), ReadFormButton('', k='2'), ReadFormButton('', k='3')],
+    [ReadFormButton('O', k='1'), ReadFormButton('', k='2'), ReadFormButton('', k='3')],
     [ReadFormButton('', k='4'), ReadFormButton('', k='5'), ReadFormButton('', k='6')],
     [ReadFormButton('', k='7'), ReadFormButton('', k='8'), ReadFormButton('', k='9')],
     [Txt('' * 10)],
@@ -12,20 +12,28 @@ layout = [
 ]
 Windo = FlexForm('Jogo', default_button_element_size=(5, 2),  auto_size_buttons=False, layout=layout)
 
-bot = random.randint(1, 9)
-   
+rodadas = 0
+
 while True:
+    rodadas += 1
+
     button, values = Windo.Read()
 
+    bot = randint(1, 9)
 
     if button == None:
         break
 
-    if button == 'subm':
+    elif button == 'subm':
         popup('O Adversario fez a sua jagada')
-
-#    if bot:
-#        Windo.find_element(bot).update('O'),  Em teste
-
+        rodadas = 0
+        if bot:
+            leitor = str(int(float(bot)))
+            print(leitor)
+            Windo.find_element(leitor).update('O')
+            
     if button:
-        Windo.find_element(button).update('X')
+        if rodadas > 1:
+            popup('nao pode mais jogar aprete no subit')
+        else:
+            Windo.find_element(button).update('X')
