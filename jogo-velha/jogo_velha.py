522ff2a563d44b93a55ebd4411692a4ff3740edc
Davi Aredes <daviaredes@gmail.com>
2023-01-18 18:44:42 -0300

    pequena mudança layout

--- jogo-velha/jogo_velha.py
+++ jogo-velha/jogo_velha.py
@@ -4,14 +4,19 @@ from random import randint
 theme('DarkGrey14')
 
 layout = [
-    [ReadFormButton('O', k='1'), ReadFormButton('', k='2'), ReadFormButton('', k='3')],
-    [ReadFormButton('', k='4'), ReadFormButton('', k='5'), ReadFormButton('', k='6')],
-    [ReadFormButton('', k='7'), ReadFormButton('', k='8'), ReadFormButton('', k='9')],
     [Txt('' * 10)],
-    [Button(k='subm')]
+    [Button('', k='1'), Button('', k='2'), Button('', k='3')],
+    [Button('', k='4'), Button('', k='5'), Button('', k='6')],
+    [Button('', k='7'), Button('', k='8'), Button('', k='9')],
+    [Txt('' * 10)],
+    [Button('jogar', k='subm')]
 ]
 Windo = FlexForm('Jogo', default_button_element_size=(5, 2),  auto_size_buttons=False, layout=layout)
 
+# ideia 
+historico_player = [] 
+historico_bot = []
+# ^^^^^^^^^^^^
 rodadas = 0
 
 while True:
@@ -19,21 +24,23 @@ while True:
 
     button, values = Windo.Read()
 
-    bot = randint(1, 9)
 
     if button == None:
         break
 
     elif button == 'subm':
-        popup('O Adversario fez a sua jagada')
+        bot = randint(1, 9)
+        # historico_bot.append(bot)
         rodadas = 0
         if bot:
             leitor = str(int(float(bot)))
             print(leitor)
             Windo.find_element(leitor).update('O')
-            
+    
     if button:
         if rodadas > 1:
-            popup('nao pode mais jogar aprete no subit')
+            popup('nao pode mais jogar aperte em jogar ')
         else:
+            # historico_player.append(button)
             Windo.find_element(button).update('X')
+            Windo.find_element('subm').update('jogar')
