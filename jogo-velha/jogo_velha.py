16c5674df0eb6b4cb5c19c8c02eb1a80b41567cd
Davi Aredes <daviaredes@gmail.com>
2023-02-10 12:26:23 -0300

    Frist Day

--- jogo-velha/jogo_velha.py
+++ jogo-velha/jogo_velha.py
@@ -1,46 +1,40 @@
 from PySimpleGUI import *
-from random import randint
 
-theme('DarkGrey14')
+def layout_main():
+    theme('DarkGrey14')
+    layout = [
+        [Txt('' * 10)],
+        [Txt('', size=(4, 0)), Text('  BEM VINDO')],
+        [Txt('', size=(7, 0)), Text(' AO')],
+        [Txt('', size=(3, 0)), Text('JOGO DA VELHA')],
+        [Txt('' * 10)],
+        [Text('ESCOLHA O MODO DE JOGO')],
+        [Txt('', size=(3, 0)), Button('1 player'), Button('2 player')],
+        [Txt('' * 10)]
+    ]
+    return Window('INiCIO', layout=layout, font=('italic'), finalize=True, resizable=True)
 
-layout = [
-    [Txt('' * 10)],
-    [Button('', k='1'), Button('', k='2'), Button('', k='3')],
-    [Button('', k='4'), Button('', k='5'), Button('', k='6')],
-    [Button('', k='7'), Button('', k='8'), Button('', k='9')],
-    [Txt('' * 10)],
-    [Button('jogar', k='subm')]
-]
-Windo = FlexForm('Jogo', default_button_element_size=(5, 2),  auto_size_buttons=False, layout=layout)
 
-# ideia 
-historico_player = [] 
-historico_bot = []
-# ^^^^^^^^^^^^
-rodadas = 0
+def player_vs_bot():
+    theme('DarkGrey14')
+    layout = [
+        [Txt('' * 10)],
+        [Button('', k='1'), Button('', k='2'), Button('', k='3')], 
+        [Button('', k='4'), Button('', k='5'), Button('', k='6')],
+        [Button('', k='7'), Button('', k='8'), Button('', k='9')], 
+        [Txt('' * 10)]
+    ]
+    return Window('Jogo', layout=layout, default_button_element_size=(10, 3),  auto_size_buttons=False, finalize=True, resizable=True)
+
+
+Window1, Window2 = layout_main(), None
 
 while True:
-    rodadas += 1
+    window, event, values  = read_all_windows()
 
-    button, values = Windo.Read()
-
-
-    if button == None:
+    if window == Window1 and event == WIN_CLOSED:
         break
 
-    elif button == 'subm':
-        bot = randint(1, 9)
-        # historico_bot.append(bot)
-        rodadas = 0
-        if bot:
-            leitor = str(int(float(bot)))
-            print(leitor)
-            Windo.find_element(leitor).update('O')
-    
-    if button:
-        if rodadas > 1:
-            popup('nao pode mais jogar aperte em jogar ')
-        else:
-            # historico_player.append(button)
-            Windo.find_element(button).update('X')
-            Windo.find_element('subm').update('jogar')
+    if window == Window1 and event == '1 player':
+        Window2 == player_vs_bot()
+        Window1.hide()
