63f2d5cbf33d96d9f52417efc923e56efd2b3686
Davi Aredes <daviaredes@gmail.com>
2023-02-13 23:16:02 -0300

    small modification day 2

--- jogo-velha/jogo_velha.py
+++ jogo-velha/jogo_velha.py
@@ -27,14 +27,30 @@ def player_vs_bot():
     return Window('Jogo', layout=layout, default_button_element_size=(10, 3),  auto_size_buttons=False, finalize=True, resizable=True)
 
 
-Window1, Window2 = layout_main(), None
+def player_vs_player(): # test
+    theme('DarkGrey14')
+    layout = [
+        [Txt('' * 10)],
+        [Button('1', k='1'), Button('4', k='2'), Button('7', k='3')], 
+        [Button('2', k='4'), Button('5', k='5'), Button('8', k='6')],
+        [Button('3', k='7'), Button('6', k='8'), Button('9', k='9')], 
+        [Txt('' * 10)]
+    ]
+    return Window('Jogo2', layout=layout, default_button_element_size=(10, 3),  auto_size_buttons=False, finalize=True, resizable=True)
+
+
+Window1, Window2, Window3 = layout_main(), None, None
 
 while True:
     window, event, values  = read_all_windows()
 
-    if window == Window1 and event == WIN_CLOSED:
+    if event == WIN_CLOSED or event == 'Exit':
         break
 
     if window == Window1 and event == '1 player':
         Window2 == player_vs_bot()
         Window1.hide()
+    elif window == Window1 and event == '2 player':
+        Window3 == player_vs_player()
+        Window1.hide()
+    
