3350e7b99f6146f45c7140c4659c3990db20c902
Davi Aredes <daviaredes@gmail.com>
2023-03-27 22:34:54 -0300

    tests,final display table

--- tic_tac_toe/tic _tac_toe.py
+++ tic_tac_toe/tic _tac_toe.py
@@ -4,15 +4,14 @@ def start():
     global scorer
     print('Welcome to Tic Tac Toe\n ___|___|___ \n ___|___|___\n    |   |   ')
     print('If you don t know how to play, type 3\nBut first choose your marking 1 == X or 2 == O')
-    if rounds == 0:
-        try:
-            scorer = int(input('1 or 2: '))
-            if scorer > 3:
-                print('\nNo to this option, Choose 1 = X 2 = O or 3 = tutorial\n')
-                start()
-        except:
+    try:
+        scorer = int(input('1 or 2: '))
+        if scorer > 3:
             print('\nNo to this option, Choose 1 = X 2 = O or 3 = tutorial\n')
             start()
+    except:
+        print('\nNo to this option, Choose 1 = X 2 = O or 3 = tutorial\n')
+        start()
 
 
 def bot(board, bot_option):
@@ -22,7 +21,6 @@ def bot(board, bot_option):
 
 
 def main():
-    global rounds
     board = [[], [], [], [], [], [], [], [], []]
     start()
 
@@ -31,13 +29,12 @@ def main():
         if scorer == 3:           
             print('\n  Well, basically the 9 positions of your choice win whoever has 3 X or 3 O in the same direction, horizontally, vertically or diagonally \n')
             print(' _1_|_2_|_3_ \n _4_|_5_|_6_\n  7 | 8 | 9 \n')
-        
             start()
             continue
         else:
             try:
                 player_move = int(input('what position: '))
-                rounds += 1
+                
                 if scorer == 1:
                     board[player_move - 1] = 'X'
                     bot_option = 'O'
@@ -49,6 +46,7 @@ def main():
                 continue
 
         bot(board, bot_option)
-        print(board)
-
+        print(f' _{board[0]}_|_{board[1]}_|_{board[2]}_ \n _{board[3]}_|_{board[4]}_|_{board[5]}_\n  {board[6]} | {board[7]} | {board[8]} \n')
+    
 main()
+ 
