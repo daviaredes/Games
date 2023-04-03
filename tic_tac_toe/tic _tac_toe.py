906a569aa33634a9bf01bf1544ad0f093a2975ff
Davi Aredes <daviaredes@gmail.com>
2023-04-03 17:09:18 -0300

    correction of some bugs, repositioning of some codes

--- tic_tac_toe/tic _tac_toe.py
+++ tic_tac_toe/tic _tac_toe.py
@@ -6,61 +6,65 @@ def start():
     print('If you don t know how to play, type 3\nBut first choose your marking 1 == X or 2 == O')
     try:
         scorer = int(input('1 or 2: '))
-        if scorer > 3:
+        if scorer < 1 or scorer > 3:
             print('\nNo to this option, Choose 1 = X 2 = O or 3 = tutorial\n')
+            start()
+        elif scorer == 3:           
+            print('\n  Well, basically the 9 positions of your choice win whoever has 3 X or 3 O in the same direction, horizontally, vertically or diagonally \n')
+            print(' _1_|_2_|_3_ \n _4_|_5_|_6_\n  7 | 8 | 9 \n')
             start()
     except:
         print('\nNo to this option, Choose 1 = X 2 = O or 3 = tutorial\n')
         start()
 
 
-def bot(board, board_bot, bot_option):
+def bot(board, bot_option):
     from random import randint
-    bot = randint(1, 9)
-    l = len(board[bot - 1])
-    if l == 1:
-        bot = randint(1, 9)
+    opponent_bot = randint(1, 9)
+    checker_bot = len(board[opponent_bot - 1])
+    if checker_bot == 1:
+       print('bot e igaul')
+       pass
     else:
-        board_bot[bot - 1] = bot_option
+        board[opponent_bot- 1] = bot_option
         pass
 
 
 def main():
     board = [[], [], [], [], [], [], [], [], []]
-    board_bot = [[], [], [], [], [], [], [], [], []]
 
     start()
 
     while True:
-
-        if scorer == 3:           
-            print('\n  Well, basically the 9 positions of your choice win whoever has 3 X or 3 O in the same direction, horizontally, vertically or diagonally \n')
-            print(' _1_|_2_|_3_ \n _4_|_5_|_6_\n  7 | 8 | 9 \n')
-            start()
-            continue
-        else:
-            try:
-                player_move = int(input('what position: '))
-                
-                if scorer == 1:
-                    board[player_move - 1] = 'X'
-                    bot_option = 'O'
-                elif scorer == 2:
-                    board[player_move - 1] = 'O'
-                    bot_option = 'X'
-                    
-                if board[0] and board[1] and board[2] == 'X' or board[3] and board[4] and board[5] == 'X' or board[6] and board[7] and board[8] == 'X':
-                    print('X ganhou')
-                    break
-                elif board[0] and board[1] and board[2] == 'O' or board[3] and board[4] and board[5] == 'O' or board[6] and board[7] and board[8] == 'O':
-                    print('O ganhou')
-                    break
-            except:
-                print('\n   No to this option, the choice goes from 1 to 9 only\n')
+        try:
+            player_move = int(input('what position: '))
+            if player_move < 1 or player_move > 9:
+                print('No to this option, the choice goes from 1 to 9 only')
                 continue
+            checker_player = len(board[player_move - 1])
+            if checker_player == 1:
+                print('Has already been played in this house try another one')
+                continue
+            
+            if scorer == 1:
+                board[player_move - 1] = 'X'
+                bot_option = 'O'
+            elif scorer == 2:
+                board[player_move - 1] = 'O'
+                bot_option = 'X'
+                
+            if board[0] and board[1] and board[2] == 'X' or board[3] and board[4] and board[5] == 'X' or board[6] and board[7] and board[8] == 'X':
+                print('X ganhou')
+                break
+            elif board[0] == 'O' and board[1] == 'O' and board[2] == 'O' or board[3] == 'O' and board[4] == 'O' and board[5] == 'O' or board[6] == 'O' and board[7] == 'O' and board[8] == 'O':
+                print('O ganhou')
+                break
 
-        bot(board, board_bot, bot_option)
-        board = board_bot
+        except:
+            print('\n   No to this option, the choice goes from 1 to 9 only\n')
+            continue
+
+        bot(board, bot_option)
         print(board)
 
 main()
