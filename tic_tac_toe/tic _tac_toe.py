ad9e3c794c19ad5f6797fafab69b7bf794bdbd2d
Davi Aredes <daviaredes@gmail.com>
2023-05-04 15:54:41 -0300

    Minor bug fixes

--- tic_tac_toe/tic _tac_toe.py
+++ tic_tac_toe/tic _tac_toe.py
@@ -59,19 +59,22 @@ def main():
         elif scorer == 2:
             board[player_move - 1] = 'O'
             bot_option = 'X'
+            
         try:     
             bot(bot_option)
         except RecursionError:
             print('Tie, neither O won nor X won, try again and defeat me')
             break
 
-        print(f' {board[0]} {board[1]} {board[2]} \n {board[3]} {board[4]} {board[5]} \n {board[6]} {board[7]} {board[8]}') 
+        print(f' {board[0]}||{board[1]}||{board[2]}')
+        print(f' {board[3]}||{board[4]}||{board[5]}')
+        print(f' {board[6]}||{board[7]}||{board[8]}')
        
             # Horizontally
         if board[0] =='X' and board[3] =='X' and board[6] =='X' or board[1] =='X' and board[4] =='X' and board[7] =='X' or board[2] =='X' and board[5] =='X' and board[8] =='X':
             print('Congratulations X won, come back and play whenever you want')
             break
-        elif board[0] =='O' and board[1] =='O' and board[2] =='O' or board[3] =='O' and board[4] =='O' and board[5] =='O' or board[6] =='O' and board[7] =='O' and board[8] =='O':
+        elif board[0] =='O' and board[3] =='O' and board[6] =='O' or board[1] =='O' and board[4] =='O' and board[7] =='O' or board[2] =='O' and board[5] =='O' and board[8] =='O':
             print('Congratulations O won, come back and play whenever you want')
             break
             # Vertically
@@ -88,5 +91,5 @@ def main():
         elif board[0] == 'O'  and  board[4] == 'O' and  board[8] == 'O' or  board[2] == 'O' and board[4] == 'O' and board[6] == 'O':
             print('Congratulations O won, come back and play whenever you want')
             break
-
+    
 main()
